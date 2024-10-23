b757f179331be34c211cd8cc9a08368b517e14ab
Samyam-gowda <samyamgowda2003@gmail.com>
2024-10-23 14:51:39 +0530

    updated

--- 5.arrays/reverse.java
+++ 5.arrays/reverse.java
@@ -1,5 +1,23 @@
-package 5.arrays;
+// package 5.arrays;
 
 public class reverse {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6,7,8};
+        int left = 0 ;
+        int right = arr.length-1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+    }
+
+    for(int i = 0 ; i < arr.length ;i++){
+        System.out.print(" " + arr[i]);
+    }
+
+}
     
 }
